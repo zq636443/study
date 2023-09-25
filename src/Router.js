32ac87924f4b2b691d3ca9f9e2254ba7e8e27d44
chenshuimu <623242742@qq.com>
2023-09-25 11:52:17 +0800

    gx

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+// AppRouter.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index/Index';
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* 添加其他路由规则 */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
---
+++ src/pages/Index/Index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Main(params) {
+  
+  return (
+    <div>1</div>
+  )
+}
+export default Main
